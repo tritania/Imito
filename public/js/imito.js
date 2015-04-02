bb28c63774fe7887d60832b6aa95f8bcb69cb330
Tritania <erikwilson@magnorum.com>
2015-04-02 15:18:13 -0300

    Fixed some css, added leap controled square.

--- public/js/imito.js
+++ public/js/imito.js
@@ -1,13 +1,21 @@
 var width = document.documentElement.clientWidth,
     height = window.innerHeight,
-    game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update,  }, false, false),
+    game = new Phaser.Game(width, height, Phaser.AUTO, 'gamec', { preload: preload, create: create, update: update,  }, false, false),
+    level = 0,
     active = false,
     guessing = false,
     showing = false,
-    slots = [];
-
+    current,
+    slots = [], //holds position of slots
+    shapes = [], //position of shapes
+    guess = [], //players guesses
+    actual = [], //actual values
+    types = ["1","2","3","4","5","6","7","8","9","10"],
+    color = ["1","2","3","4","5","6","7","8","9","10"], //use sprite.tint to change color
+    grabed;
+ 
 function preload() {
-    game.load.spritesheet('1', 'assets/S1.png', 200, 200);
+    game.load.spritesheet('1', 'assets/S1.png', 200, 200); //only need shapes
     game.load.spritesheet('2', 'assets/S2.png', 200, 200);
     game.load.spritesheet('slot', 'assets/slot.png', 210, 210);
     getSlots();
@@ -21,10 +29,28 @@ function create() {
         game.add.sprite(slots[i].x, slots[i].y, 'slot')   
     }
     
+    grabed = game.add.sprite(30, 30, '1')
+    grabed.anchor.setTo(0.5, 0.5);
 }
 
 function update() {
 
+}
+
+function moveGrabbed(position, roll) {
+    grabed.x = position[0];
+    grabed.y = position[1];
+    grabed.rotation = roll * -1;
+}
+
+Leap.loop(function (frame) {
+    frame.hands.forEach(function(hand, index) {
+        moveGrabbed(hand.screenPosition(), hand.roll());
+    });
+}).use('screenPosition', { scale: 0.25 });
+
+function createPattern() {
+    Math.floor((Math.random() * 10) + 0);
 }
 
 function getSlots() {
@@ -34,13 +60,15 @@ function getSlots() {
         gaps = totalSlots - 1,
         size = (totalSlots * slotSize) + (gaps * gapSize),
         startingXPos = (width - size) / 2,
-        startingYPos = (height - slotSize)  / 2 - 110,
+        startingYPos = (height - slotSize)  / 2,
         i = 0;
     
     for (i; i < totalSlots; i++) {
         var x = startingXPos + (i * (slotSize + gapSize));
         slots[i] = { x: x, y: startingYPos };
-    }
-    
+        shapes[i] = { x: x - 5, y: startingYPos - 5 };
+    }    
         
 }
+
+Leap.loopController.setBackground(true)
